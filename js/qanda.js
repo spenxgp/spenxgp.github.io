0890f4de67cfb7bf76c0585a950e027c7021394d
SPX <spenxgp@hotmail.com>
2016-08-24 21:29:44 +0000

    draft

--- js/qanda.js
+++ js/qanda.js
@@ -1,4 +1,30 @@
 // JavaScript File
 //Switch statment .....
-var = answer;
-switch ()
+
+
+/*if (document.getElementById("ans1").innerHTML = "BUENOS DIAS!") {
+    document.getElementById("ans1").innerHTML = "Yes, that's right";
+} else {document.getElementById("ans1").innerHTML = "That's not right, it's BUENOS DIAS";
+
+} 
+
+onclick  submit if else ?
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo">Click the button to change the layout of this paragraph</p>
+
+<script>
+function myFunction() {
+    var x = document.getElementById("demo");
+    x.style.fontSize = "25px"; 
+    x.style.color = "red"; 
+}
+</script>
+
+<button onclick="myFunction()">Click Me!</button>
+
+</body>
+</html>*/
